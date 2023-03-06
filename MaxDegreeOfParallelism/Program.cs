507bd924f6c0ed188ab16068b0231493fe069775
Alexandre Machado <alexandre@machado.cc>
2023-03-06 18:27:08 -0300

    add MaxDegreeOfParallelism tests

---
+++ MaxDegreeOfParallelism/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+var locker = new Object();
+int count = 0;
+var countFinished = 0;
+var loopNumber = 1000;
+var stopwatch = Stopwatch.StartNew();
+Console.WriteLine("Environment.ProcessorCount: " + Environment.ProcessorCount);
+
+Parallel.For
+    (0
+     , loopNumber
+     , new ParallelOptions { MaxDegreeOfParallelism = 20 }
+     , (i) =>
+           {
+               Interlocked.Increment(ref count);
+               lock (locker)
+               {
+                   Console.WriteLine($"Number of active threads: {count}, Elapsed Time: {stopwatch.Elapsed} finished: {((decimal)countFinished/(decimal)loopNumber):P}");
+                   Thread.Sleep(10);
+               }
+               Interlocked.Decrement(ref count);
+               Interlocked.Increment(ref countFinished);
+           }
+    );
